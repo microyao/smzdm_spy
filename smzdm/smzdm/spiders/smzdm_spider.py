89f697933474d99ba450beb8f3a8b5f7a944919b
yaocheng <microyao0825@gmail.com>
2015-09-14 22:55:38 +0800

    title:add prase,next step: cookie:wq

--- smzdm/smzdm/spiders/smzdm_spider.py
+++ smzdm/smzdm/spiders/smzdm_spider.py
@@ -4,3 +4,8 @@ class SmzdmSpider(scrapy.Spider):
     name = "smzdm"
     allowed_domains = ["smzdm.com"]
     start_urls = {"http://www.smzdm.com"}
+
+    def parse(self,response):
+        filename = "ps4.txt"
+        with open(filename,"wb") as f:
+            f.write(response.body)
