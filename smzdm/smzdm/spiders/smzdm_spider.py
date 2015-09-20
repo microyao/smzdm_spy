76635dda07f13dd5a58d0c7bd3aef01047464db1
yaocheng <microyao0825@gmail.com>
2015-09-20 22:58:35 +0800

    title: write data to item.json

--- smzdm/smzdm/spiders/smzdm_spider.py
+++ smzdm/smzdm/spiders/smzdm_spider.py
@@ -1,14 +1,17 @@
 import scrapy
-
+from smzdm.items import SmzdmItem
 class SmzdmSpider(scrapy.Spider):
     name = "smzdm"
     allowed_domains = ["smzdm.com"]
     start_urls = {"http://www.smzdm.com"}
 
     def parse(self,response):
-        filename = "ps4.txt"
-        with open(filename,"ab") as f:
-            for sel in response.xpath('//ul/li'):
-                link = sel.xpath('a/@href').extract()
-                #f.write(link)
-                print link
+#        filename = "ps4.txt"
+#        with open(filename,"ab") as f:
+        for sel in response.xpath('//ul/li'):
+            item = SmzdmItem()
+            item['link'] = sel.xpath('a/@href').extract()
+            
+            #f.write(link)
+            yield item
+                
